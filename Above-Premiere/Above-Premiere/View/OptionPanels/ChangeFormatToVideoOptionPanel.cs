b52d0ee3232c0182bdb84d8664420fffff345da3
Matias Torrez <matias.torrez@davinci.edu.ar>
2021-09-29 02:40:28 -0300

    Creando la primer interfaz de opciones del menu (Cambiar formato de video)

---
+++ Above-Premiere/Above-Premiere/View/OptionPanels/ChangeFormatToVideoOptionPanel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Above_Premiere.View.OptionPanels
+{
+    public class ChangeFormatToVideoOptionPanel : OptionPanel
+    {
+        const string TITLE = "Cambiar formato a un video";
+        private Label LabelComboBox;
+        private ComboBox ComboBoxVideoFormat;
+
+        public ChangeFormatToVideoOptionPanel() : base(TITLE)
+        {
+            this.LabelComboBox = new Label();
+            this.ComboBoxVideoFormat = new ComboBox();
+
+
+        }
+
+        public override void setOwnStyles()
+        {
+            this.TitlePanelHeader.Location = new System.Drawing.Point(220, 22);
+            this.TitlePanelHeader.Size = new System.Drawing.Size(277, 25);
+
+            this.TextBoxPathVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxPathVideo.Location = new System.Drawing.Point(177, 27);
+            this.TextBoxPathVideo.ReadOnly = true;
+            this.TextBoxPathVideo.Size = new System.Drawing.Size(251, 26);
+            this.TextBoxPathVideo.TabIndex = 0;
+
+
+            this.ButtonSearchVideo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSearchVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSearchVideo.Location = new System.Drawing.Point(434, 27);
+            this.ButtonSearchVideo.Size = new System.Drawing.Size(75, 26);
+            this.ButtonSearchVideo.TabIndex = 1;
+            this.ButtonSearchVideo.Text = "Search";
+            this.ButtonSearchVideo.UseVisualStyleBackColor = true;
+
+
+            this.LabelTextBoxNewName.AutoSize = true;
+            this.LabelTextBoxNewName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTextBoxNewName.Location = new System.Drawing.Point(110, 149);
+            this.LabelTextBoxNewName.Size = new System.Drawing.Size(251, 36);
+            this.LabelTextBoxNewName.TabIndex = 4;
+            this.LabelTextBoxNewName.Text = "Coloque el nombre que quiera \r\nque tenga el video a transformar";
+
+            this.LabelComboBox.AutoSize = true;
+            this.LabelComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelComboBox.Location = new System.Drawing.Point(110, 102);
+            this.LabelComboBox.Size = new System.Drawing.Size(238, 18);
+            this.LabelComboBox.TabIndex = 2;
+            this.LabelComboBox.Text = "Seleccione formato que quiera";
+
+
+            this.ComboBoxVideoFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxVideoFormat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxVideoFormat.FormattingEnabled = true;
+            this.ComboBoxVideoFormat.Items.AddRange(new object[] {
+            "mp4",
+            "avi",
+            "mpeg",
+            "flv"});
+            this.ComboBoxVideoFormat.Location = new System.Drawing.Point(388, 99);
+            this.ComboBoxVideoFormat.Size = new System.Drawing.Size(165, 26);
+            this.ComboBoxVideoFormat.TabIndex = 3;
+
+
+            this.TextBoxNewName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxNewName.Location = new System.Drawing.Point(388, 159);
+            this.TextBoxNewName.Size = new System.Drawing.Size(251, 26);
+            this.TextBoxNewName.TabIndex = 5;
+
+
+            this.ButtonExecute.Location = new System.Drawing.Point(282, 233);
+            this.ButtonExecute.Size = new System.Drawing.Size(146, 25);
+            this.ButtonExecute.TabIndex = 6;
+            this.ButtonExecute.Text = "Convetir";
+            this.ButtonExecute.UseVisualStyleBackColor = true;
+
+
+
+        }
+
+
+     
+    }
+}
---
+++ Above-Premiere/Above-Premiere/View/OptionPanels/OptionPanel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Above_Premiere.View.OptionPanels
+{
+     abstract public class OptionPanel : Panel
+    {
+
+        protected Button ButtonExecute;
+        protected TextBox TextBoxNewName;
+        protected Label LabelTextBoxNewName;
+        protected TextBox TextBoxPathVideo;
+        protected Button ButtonSearchVideo;
+        protected Panel HeaderPanel;
+        protected Label TitlePanelHeader;
+        protected Panel BodyPanel;
+        protected OpenFileDialog openFileDialog1;
+
+
+        public OptionPanel(string title)
+        {
+            ButtonExecute = new Button();
+            TextBoxNewName = new TextBox();
+            LabelTextBoxNewName = new Label();
+            TextBoxPathVideo = new TextBox();
+            ButtonSearchVideo = new Button();
+            HeaderPanel = new Panel();
+            TitlePanelHeader = new Label();
+            TitlePanelHeader.Text = title;
+            BodyPanel = new Panel();
+            openFileDialog1 = new OpenFileDialog();
+
+            setStyles();
+        }
+
+        public void setStyles()
+        {
+            this.Controls.Add(HeaderPanel);
+            this.Controls.Add(BodyPanel);
+
+           
+
+
+            this.Location = new System.Drawing.Point(0, 90);
+            this.Size = new System.Drawing.Size(900, 360);
+            this.TabIndex = 1;
+
+
+            this.HeaderPanel.Controls.Add(TitlePanelHeader);
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.HeaderPanel.Size = new System.Drawing.Size(728, 72);
+            this.HeaderPanel.TabIndex = 0;
+
+            this.TitlePanelHeader.AutoSize = true;
+            this.TitlePanelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitlePanelHeader.TabIndex = 0;
+
+
+            this.BodyPanel.Controls.Add(ButtonExecute);
+            this.BodyPanel.Controls.Add(TextBoxNewName);
+            this.BodyPanel.Controls.Add(LabelTextBoxNewName);
+            this.BodyPanel.Controls.Add(TextBoxPathVideo);
+            this.BodyPanel.Controls.Add(ButtonSearchVideo);
+            this.BodyPanel.Controls.Add(HeaderPanel);
+            this.BodyPanel.Controls.Add(TitlePanelHeader);
+            this.BodyPanel.Location = new System.Drawing.Point(0, 69);
+            this.BodyPanel.Size = new System.Drawing.Size(728, 291);
+            this.BodyPanel.TabIndex = 1;
+
+            this.ButtonSearchVideo.Click += new System.EventHandler(this.ButtonSearchVideo_Click);
+
+
+        }
+
+        private void setPathInTextBox()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TextBoxPathVideo.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void ButtonSearchVideo_Click(object sender, EventArgs e)
+        {
+            setPathInTextBox();
+        }
+
+        public abstract void setOwnStyles();
+    }
+}
